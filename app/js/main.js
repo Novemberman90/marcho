ffc426be7a9fffe5568d06285724e1cf6bd6a712
Alexey <alexledovit@gmail.com>
2024-05-02 19:47:24 +0300

    add shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,34 @@
 $(function() {
+
+
+  $('.shop-content__filter-btn').on('click', function() {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active')
+      $(this).addClass('shop-content__filter-btn--active')
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list')
+  });
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list')
+  });
+
+  $('.select-style').styler({});
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+     prefix: "$",
+     onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+     },
+     onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+      /* $('.filter-price__from').text(data.from) - так я указываю атрибут у класса с какого эоемента нужно принимать значение */
+     }
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
